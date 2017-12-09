5336bae6560443592a5b9d035acd3c26ef1281ac
chenxuan <chenxuan@kongzhongjr.com>
2017-12-09 22:18:09 +0800

    :octocat: npm 1.0.6 :zap: 添加判断设备和微信浏览器的api

--- src/device/mobileType.js
+++ src/device/mobileType.js
@@ -2,10 +2,10 @@
  * @desc 获取操作系统类型
  * @return {String}
  */
-function mobileType () {
+const mobileType = () => {
   let u = navigator.userAgent
-  let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 //android终端
-  let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) //ios终端
+  let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1  //android终端
+  let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)  //ios终端
   if (isAndroid) {
     return 'android'
   } else if (isiOS) {
