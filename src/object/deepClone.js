8d96bbe70eefaecf49e77a5b032b1ea400512bc4
chenxuan <chenxuan@kongzhongjr.com>
2017-12-10 01:46:09 +0800

    :elephant: deepClone方法更新

--- src/object/deepClone.js
+++ src/object/deepClone.js
@@ -1,35 +1,21 @@
 /**
  * @desc 深拷贝，支持常见类型
- * @param {Any} values
+ * @param {Object} obj
  */
-function deepClone(values) {
-    var copy;
-
-    if (null == values || "object" != typeof values) return values;
-
-    if (values instanceof Date) {
-        copy = new Date();
-        copy.setTime(values.getTime());
-        return copy;
+const deepClone = (obj) => {
+  let str
+  let newobj = obj.constructor === Array ? [] : {}
+  if (typeof obj !== 'object') {
+    return
+  } else if (window.JSON) {
+    str = JSON.stringify(obj) //系列化对象
+    newobj = JSON.parse(str) //还原
+  } else {
+    for (var i in obj) {
+      newobj[i] = typeof obj[i] === 'object' ? deepClone(obj[i]) : obj[i]
     }
-
-    if (values instanceof Array) {
-        copy = [];
-        for (var i = 0, len = values.length; i < len; i++) {
-            copy[i] = deepClone(values[i]);
-        }
-        return copy;
-    }
-
-    if (values instanceof Object) {
-        copy = {};
-        for (var attr in values) {
-            if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
-        }
-        return copy;
-    }
-
-    throw new Error("Unable to copy values! Its type isn't supported.");
+  }
+  return newobj
 }
 
 module.exports = deepClone
