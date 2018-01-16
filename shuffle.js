daca234ac5166b553b6240cc5289a7920f76b585
chenxuan <chenxuan@kongzhongjr.com>
2018-01-17 00:58:24 +0800

    :deciduous_tree: animationFrame更新 npm 1.1.8

--- shuffle.js
+++ shuffle.js
@@ -3,16 +3,21 @@
  * @param {Array} arr
  * @return {Array} res
  */
-var getRandom = require('../random/getRandom');
+// const getRandom = require('../random/getRandom')
+// const shuffle = (arr) => {
+//   let _arr = arr.slice()
+//   for (let i = 0, len = _arr.length; i < len; i++) {
+//     let j = getRandom(0, i)
+//     let t = _arr[i]
+//     _arr[i] = _arr[j]
+//     _arr[j] = t
+//   }
+//   return _arr
+// };
 var shuffle = function shuffle(arr) {
-  var _arr = arr.slice();
-  for (var i = 0, len = _arr.length; i < len; i++) {
-    var j = getRandom(0, i);
-    var t = _arr[i];
-    _arr[i] = _arr[j];
-    _arr[j] = t;
-  }
-  return _arr;
+  return arr.sort(function () {
+    return 0.5 - Math.random();
+  });
 };
 
 module.exports = shuffle;
