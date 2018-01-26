b5aae74112cbe46beeedbd7c1cc9ca6625c07094
chenxuan <chenxuan@kongzhongjr.com>
2018-01-26 15:47:25 +0800

    :alien: 优化 shuffle 随机乱序函数

--- shuffle.js
+++ shuffle.js
@@ -3,21 +3,16 @@
  * @param {Array} arr
  * @return {Array} res
  */
-// const getRandom = require('../random/getRandom')
-// const shuffle = (arr) => {
-//   let _arr = arr.slice()
-//   for (let i = 0, len = _arr.length; i < len; i++) {
-//     let j = getRandom(0, i)
-//     let t = _arr[i]
-//     _arr[i] = _arr[j]
-//     _arr[j] = t
-//   }
-//   return _arr
-// };
+var getRandom = require('../random/getRandom');
 var shuffle = function shuffle(arr) {
-  return arr.sort(function () {
-    return 0.5 - Math.random();
-  });
+  var _arr = arr.slice();
+  for (var i = 0, len = _arr.length; i < len; i++) {
+    var j = getRandom(0, i);
+    var t = _arr[i];
+    _arr[i] = _arr[j];
+    _arr[j] = t;
+  }
+  return _arr;
 };
 
 module.exports = shuffle;
